0aadbb5a5531a2b50938f540a91dc90234dd1eae
divas sharma <divaspdl44@gmail.com>
2024-04-28 12:57:14 +0545

    loading and error chnaged

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,6 +12,8 @@ function Login() {
     const dispatch=useDispatch();
     const navigate=useNavigate()
     const [loading, setLoading]=useState(false)
+    const [error, setError]=useState("")
+    
   const {
     register,
     handleSubmit,
@@ -19,24 +21,29 @@ function Login() {
    
   } = useForm();
 
+
   const onSubmit =  async ({email, password}) => {
-    setLoading(true)
+    setError("")
+  setLoading(true)
      try {
         const session= await authService.login({email, password});
      if (session){
         const userData= await authService.getCurrentuser();
         console.log(userData)
+        
         if (userData){
          dispatch(authlogin(userData));
          navigate("/")
          toast.success("succesfully logged in ", {
           position:"top-right"
          })
-         setLoading(false)
+         
         }
     }
      } catch (error) {
-         throw error;
+         setError(error.message);
+     } finally{
+      setLoading(false)
      }
   };
   if (loading==true){
@@ -47,7 +54,8 @@ function Login() {
     <div className=" flex flex-col  w-full  mt-[60px]  h-screen  items-center    ">
       <div className="w-[400px] flex flex-col gap-[15px]  ">
         <p className="font-bold text-xl ml-[120px] "> Login</p>
-        
+        {error &&  <p className="text-red-500 text-wrap"> {error}</p> }
+       
       <Input
         {...register("email")}
         className="py-[9px] px-[7px]  rounded-md "
@@ -55,7 +63,7 @@ function Login() {
         placeholder=" eg:email@email.com"
       />
      {errors.email && (
-        <p className="text-red-500 "> {`${errors.email.message}`}</p>
+        <p className="text-red-500 ">{`${errors.email.message}`}</p>
       )}
 
       <Input
