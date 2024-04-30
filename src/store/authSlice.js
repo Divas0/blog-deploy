1e68a22af0c7d4d149bb1008ed787838842968a9
divas sharma <divaspdl44@gmail.com>
2024-04-30 23:04:42 +0545

    fixed authslice error

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -12,15 +12,16 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-   login: (state, action)=>{
-    state.status = true
-    state.userData =action.payload
-}, 
-logout: (state, action) => {
-    state.status = false
-    state.userData = null
-}
-  
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload.userData;
+  },
+  logout: (state) => {
+      state.status = false;
+      state.userData = null;
+  }
+  }
 });
 
 export const { login, logout } = authSlice.actions;
